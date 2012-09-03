02689353b5c739a308c0aa2f57c9869ee762c27d
pigats <andrea@fruktarbo.com>
2012-09-03 17:49:08 +0200

    create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pip48rails::Application.config.secret_token = 'e4a590ce0addd2c184daf06fb1fcdd17b7b26e8862e8b4e1dd83c6514fd4802a40ef5874d0743745830355d85dd8fb194dd966861abf8b1fb51c896d78706e6e'
